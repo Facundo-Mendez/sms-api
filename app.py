0f40885bd24785d722ca9898024265e96b07ef1c
Facundo-Mendez <facundo.b.mendez@gmail.com>
2025-03-13 11:09:31 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 from pymongo import MongoClient
+import os
 
 app = Flask(__name__)
 
@@ -21,5 +22,7 @@ def receive_sms():
 
     return jsonify({"status": "success", "message": "SMS received"}), 200
 
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
